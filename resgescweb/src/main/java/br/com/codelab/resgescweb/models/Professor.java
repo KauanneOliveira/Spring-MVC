abd1b8ba16e353a123011c2207940f2bca82a59c
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-04-21 08:25:12 -0300

    criação do diretório models com os arquivos da classe Professor e enum StatusProfessor.

---
+++ resgescweb/src/main/java/br/com/codelab/resgescweb/models/Professor.java
@@ -0,0 +1,61 @@
+package br.com.codelab.resgescweb.models;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+
+
+@Entity
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //quando for criado um novo registro de professor o próprio banco vai criar automaticamente um novo id, que será incremental e único e que será a chave primária
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private BigDecimal salario;
+
+    @Enumerated(EnumType.STRING)
+    private StatusProfessor statusProfessor;
+
+    public Professor() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public StatusProfessor getStatus() {
+        return statusProfessor;
+    }
+
+    public void setStatus(StatusProfessor status) {
+        this.statusProfessor = status;
+    }
+}
